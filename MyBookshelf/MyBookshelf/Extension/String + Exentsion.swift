8eb241896c562c8bbfb3d0ccafd0d83cb09d5baa
dumba0801 <dumba@channel.io>
2021-12-17 16:42:50 +0900

    [Improvement] 이미지 다운로드 익스텐션
    [description]
    이미지 다운로드에 대한 로직을 String Type Extension으로 처리해주었습니다.
    이와 관련된 이미지 캐싱 처리를 구현하였습니다.
    
    [issue-number]

--- MyBookshelf/MyBookshelf/Extension/String + Exentsion.swift
+++ MyBookshelf/MyBookshelf/Extension/String + Exentsion.swift
@@ -5,4 +5,20 @@
 //  Created by dumba on 2021/12/17.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+
+extension String {
+    func image() -> Single<UIImage?> {
+        if let image = ImageCacheManager.image(key: NSString(string: self)) {
+            return Observable<UIImage?>.just(image).asSingle()
+        }
+        
+        return APIService.shared.request(convertible: self)
+            .map{ UIImage(data: $0) }
+            .do(onSuccess: { image in
+                guard let image = image else { return }
+                ImageCacheManager.save(key: NSString(string: self), image: image)
+            })
+    }
+}
